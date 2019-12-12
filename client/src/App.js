d73ab4ea3e90cdc8df3ff8454c60a9c33de22ee0
Jora <jorakh@aod.am>
2019-12-12 17:38:30 +0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img
+              src={
+                "https://icon-library.net/images/batman-logo-icon/batman-logo-icon-25.jpg"
+              }
+              className="App-logo"
+              alt="logo"
+            />
+            <h1 className="App-title">Fib Calculator version 3</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t jorakhachatryan/multi-client:latest -t jorakhachatryan/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t jorakhachatryan/multi-server:latest -t jorakhachatryan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jorakhachatryan/multi-worker:latest -t jorakhachatryan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jorakhachatryan/multi-client:latest
+docker push jorakhachatryan/multi-server:latest
+docker push jorakhachatryan/multi-worker:latest
+
+docker push jorakhachatryan/multi-client:$SHA
+docker push jorakhachatryan/multi-server:$SHA
+docker push jorakhachatryan/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployments server=jorakhachatryan/multi-server:$SHA
+kubectl set image deployments/client-deployments client=jorakhachatryan/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=jorakhachatryan/multi-worker:$SHA
